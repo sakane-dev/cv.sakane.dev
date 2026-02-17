730a9949302d07e0f71750c4f251e7705eea3ecb
cyberust <ysakane@cyberust.com>
2026-02-17 16:16:40 +0900

    feat: set googleAnalyticsId explicitly

--- site.config.ts
+++ site.config.ts
@@ -1,8 +1,11 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  // 必須設定（IDは確認済み）
+  // 必須設定
   rootNotionPageId: '3099ee114e2c80c884d8e6f3a019f0fb',
+
+  // ▼ ここに追加してください！
+  googleAnalyticsId: 'G-ZEGF57R3HK',
 
   // サイト情報
   name: 'Yasuyuki Sakane CV',
@@ -10,11 +13,11 @@ export default siteConfig({
   author: 'Yasuyuki Sakane',
   description: 'Curriculum Vitae of Yasuyuki Sakane',
 
-  // ソーシャル設定（必要に応じてIDを入れてください）
+  // ソーシャル
   twitter: 'sakane_dev',
   github: 'sakane-dev',
   linkedin: 'yasuyukis',
 
-  // プレビュー画像機能のみ有効化
+  // プレビュー画像機能
   isPreviewImageSupportEnabled: true,
 })
