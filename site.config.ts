467317e9c16d699f19314a34ba3fdadfeacb5ed6
cyberust <ysakane@cyberust.com>
2026-02-17 16:19:46 +0900

    fix: remove invalid config

--- site.config.ts
+++ site.config.ts
@@ -4,9 +4,6 @@ export default siteConfig({
   // 必須設定
   rootNotionPageId: '3099ee114e2c80c884d8e6f3a019f0fb',
 
-  // ▼ ここに追加してください！
-  googleAnalyticsId: 'G-ZEGF57R3HK',
-
   // サイト情報
   name: 'Yasuyuki Sakane CV',
   domain: 'cv.sakane.dev',
